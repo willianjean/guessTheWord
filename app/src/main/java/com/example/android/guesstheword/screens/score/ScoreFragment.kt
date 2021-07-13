8f306243d98ec74989ceabececb0e2ed9675c08a
Willian Jean <willianjean@gmail.com>
2021-07-13 11:08:46 -0400

    Update to guess the word game with viewModel and liveData implementation updates using best practices

--- app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
@@ -22,7 +22,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.android.guesstheword.R
 import com.example.android.guesstheword.databinding.ScoreFragmentBinding
@@ -49,10 +51,22 @@ class ScoreFragment : Fragment() {
                 false
         )
 
+        binding.playAgainButton.setOnClickListener{ viewModel.onPlayAgain() }
+
         viewModelFactory = ScoreViewModelFactory(ScoreFragmentArgs.fromBundle((requireArguments())).score)
         viewModel = ViewModelProvider(this, viewModelFactory).get(ScoreViewModel::class.java)
 
-        binding.scoreText.text = viewModel.score.toString()
+        binding.scoreViewModel = viewModel
+
+        binding.lifecycleOwner = viewLifecycleOwner
+
+
+        viewModel.eventPlayAgain.observe(viewLifecycleOwner, Observer { playAgain ->
+            if(playAgain){
+                findNavController().navigate(ScoreFragmentDirections.actionRestart())
+                viewModel.onPlayAgainComplete()
+            }
+        })
 
         return binding.root
     }
--- app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModelFactory.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModelFactory.kt
@@ -4,9 +4,9 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import java.lang.IllegalArgumentException
 
-class ScoreViewModelFactory (private val finalScore: Int): ViewModelProvider.Factory {
+class ScoreViewModelFactory (private val finalScore: Int) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(ScoreViewModel::class.java)){
+        if (modelClass.isAssignableFrom(ScoreViewModel::class.java)) {
             return ScoreViewModel(finalScore) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
